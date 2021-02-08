01f4f68bb8ae879083a9bb385977d3110813e7f7
ycl310 <60243870+ycl310@users.noreply.github.com>
2021-02-08 14:25:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,28 +8,35 @@ f = open("names", 'r').readlines()
 parser = argparse.ArgumentParser(description="Arguments from the terminal")
 parser.add_argument('--pin', type=int, help="game pin of the kahoot session")
 parser.add_argument('--amt', type=int, help="amount of bots that join the session")
+parser.add_argument('--wait', type=int, help="time of await")
 args = parser.parse_args()
 
+pin = 0
+ite = 0
+wait = 0
+
 try:
-	pin = args.pin
-	ite = args.amt
-	print("pin: "+str(pin))
-	print("amount: "+str(ite))
-except TypeError:
+	pin = int(args.pin)
+	ite = int(args.amt)
+	wait = args.wait
+	if(wait==None):
+		wait = 0
+except TypeError as e:
 	print(str(e)+"""
 		Make sure you have passed in the required arguments,
 		use the --help flag to get information
 	""")
 	exit()
 
+
 def thread_func(name:str):
 	def joinHandle():
 		pass
 	bot = client()
-	bot.join(pin, str(name))
+	bot.join(int(pin), str(name))
 	bot.on("joined", joinHandle)
 
-for i in range(ite):
+for i in range(int(ite)):
 	x = threading.Thread(target=thread_func, args=(str(f[i]),))
 	x.start()
-	#time.sleep(1)
+	time.sleep(int(wait))
