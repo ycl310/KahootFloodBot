70ff64d08acda3be29231a6613e6d750f604d24b
ycl310 <60243870+ycl310@users.noreply.github.com>
2021-02-08 13:49:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from kahoot import client
 import threading
+import time
+
+f = open("names", 'r').readlines()
 
 def thread_func(name:str):
 	def joinHandle():
 		pass
 	bot = client()
-	bot.join(2123865, str(name))
+	bot.join(6466955, str(name))
 	bot.on("joined", joinHandle)
 
-for i in range(100):
-	x = threading.Thread(target=thread_func, args=(str(i),))
+for i in range(500):
+	x = threading.Thread(target=thread_func, args=(str(f[i]),))
 	x.start()
+	#time.sleep(1)
